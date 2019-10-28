f6fb9fef759ed85354efaecd923695b3b2e2fc0c
Challe <gabin.challe@outlook.fr>
2019-10-28 13:08:45 +0100

    Ajout du questionnaire JSON + démarrage de la partie test

--- app-questionnaire-vue/src/components/FormTest.vue
+++ app-questionnaire-vue/src/components/FormTest.vue
@@ -1,12 +1,31 @@
 <template>
-    <h1>{{msg}}</h1>
+    <div class="questionnaire">
+      <h1>{{msg}}</h1>
+      <form>
+        <p v-if="test[i] !== null"> {{ test[i]["question"] }} </p>
+        <input type="checkbox" name="option"> {{ test[i]["option1"] }}
+        <input type="checkbox" name="option"> {{ test[i]["option2"] }}
+        <input type="checkbox" name="option"> {{ test[i]["option3"] }}
+        <br/>
+      </form>
+    </div>
 </template>
 
 <script>
+import test from '../assets/questionnaire.json'
+console.log(test)
 export default {
   name: 'FormTest',
+  computed: {
+    test () {
+      return test.test.map((item) => {
+        return item
+      })
+    }
+  },
   props: {
-    msg: String
+    msg: String,
+    i: Number
   }
 }
 </script>
--- app-questionnaire-vue/src/views/Test.vue
+++ app-questionnaire-vue/src/views/Test.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="test">
     <img alt="Vue logo" src="../assets/logo.png">
-    <FormTest msg="Welcome to the Test"/>
+    <FormTest msg="Welcome to the Test" :i=i />
+    <input v-on:click="previous" type="submit" value="Précédent">
+    <input v-on:click="next" type="submit" value="Suivant">
   </div>
 </template>
 
@@ -11,8 +13,21 @@ import FormTest from '@/components/FormTest.vue'
 
 export default {
   name: 'test',
+  data () {
+    return {
+      i: 0
+    }
+  },
   components: {
     FormTest
+  },
+  methods: {
+    next: function () {
+      this.i = this.i + 1
+    },
+    previous: function () {
+      this.i = this.i - 1
+    }
   }
 }
 </script>
